d4a57c7eb6aebcb1b94ecbbf52b2883baa4deba6
likhon15 <likhon15-3916@diu.edu.bd>
2024-06-27 20:17:48 +0600

    added

--- src/app/modules/student/student.controller.ts
+++ src/app/modules/student/student.controller.ts
@@ -2,20 +2,50 @@ import { Request, Response } from 'express';
 import { StudentServices } from './student.service';
 
 const createStudent = async (req: Request, res: Response) => {
-    try {
-        const { student: studentData } = req.body;
-        const result = await StudentServices.createStudentIntoDB(studentData);
-        res.status(200).json({
-            success: true,
-            message: 'Student is created succesfully',
-            data: result,
-        });
-    } catch (err) {
-        console.log(err);
-    }
+  try {
+    const { student: studentData } = req.body;
+    const result = await StudentServices.createStudentIntoDB(studentData);
+    res.status(200).json({
+      success: true,
+      message: 'Student is created succesfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
+const getStudents = async (req: Request, res: Response) => {
+  try {
+    const result = await StudentServices.getStudentData();
+    res.status(200).json({
+      success: true,
+      message: 'Student is created succesfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const getSingleStudent = async (req: Request, res: Response) => {
+  try {
+    const { studentId } = req.params;
+
+    const result = await StudentServices.getSingleStudentFromDB(studentId);
+
+    res.status(200).json({
+      success: true,
+      message: 'Student is retrieved succesfully',
+      data: result,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 export const StudentControllers = {
-    createStudent,
+  createStudent,
+  getStudents,
+  getSingleStudent,
 };
