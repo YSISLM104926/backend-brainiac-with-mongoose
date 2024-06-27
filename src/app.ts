eacd36faf5c278f3c2519baa37c870434b488b6e
likhon15 <likhon15-3916@diu.edu.bd>
2024-06-27 19:41:35 +0600

    added

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express, { Application, Request, Response } from 'express';
 const app: Application = express();
 import cors from 'cors';
+import { StudentRoutes } from './app/modules/student/student.route';
 
 // parsers
-app.use(express());
+app.use(express.json());
 app.use(cors());
+
+app.use('/api/v1/students', StudentRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   const a = 1;
